f2c4f761411d44fc4c936aaf4decf8ddaf5992e9
Rui Vieira <revs87@msn.com>
2023-05-09 00:50:19 +0100

    usecase6: Retry

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,36 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            val numberOfRetries = 2
+            try {
+                repeat(numberOfRetries) {
+                    try {
+                        val versionsResponse = api.getRecentAndroidVersions()
+                        uiState.value = UiState.Success(versionsResponse)
+                        return@launch
+                    } catch (e: java.lang.Exception) {
+                        Timber.e(e)
+                    }
+                }
+                uiState.value = UiState.Error("Number of retries ($numberOfRetries) limit reached")
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error("Network request failed")
+            }
+        }
     }
 
 }
